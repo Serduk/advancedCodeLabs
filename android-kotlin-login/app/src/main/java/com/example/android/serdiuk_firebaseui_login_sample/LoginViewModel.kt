7b989aa46fd961febde063c17a71c7648c41fe72
Serhii Serdiuk <serduksergey90@gmail.com>
2020-12-14 19:23:23 +0200

    implement login flow for app

--- android-kotlin-login/app/src/main/java/com/example/android/serdiuk_firebaseui_login_sample/LoginViewModel.kt
+++ android-kotlin-login/app/src/main/java/com/example/android/serdiuk_firebaseui_login_sample/LoginViewModel.kt
@@ -18,24 +18,32 @@ package com.example.android.serdiuk_firebaseui_login_sample
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 import androidx.preference.PreferenceManager
 import kotlin.random.Random
 
 class LoginViewModel : ViewModel() {
+    val authenticationState = FirebaseUserLiveData().map { user ->
+        if (user != null) {
+            AuthenticationState.AUTHENTICATED
+        } else {
+            AuthenticationState.UNAUTHENTICATED
+        }
+    }
 
     companion object {
         val androidFacts = arrayOf(
-            "The first commercial Android device was launched in September 2008",
-            "The Android operating system has over 2 billion monthly active users",
-            "The first Android version (1.0) was released on September 23, 2008",
-            "The first smart phone running Android was the HTC Dream called the T-Mobile G1 in " + "some countries"
+                "The first commercial Android device was launched in September 2008",
+                "The Android operating system has over 2 billion monthly active users",
+                "The first Android version (1.0) was released on September 23, 2008",
+                "The first smart phone running Android was the HTC Dream called the T-Mobile G1 in " + "some countries"
         )
 
         val californiaFacts = arrayOf(
-            "The most populated state in the United States is California",
-            "Three out of the ten largest U. S. cities are in California",
-            "The largest tree in the world can be found in California",
-            "California became a state in 1850"
+                "The most populated state in the United States is California",
+                "Three out of the ten largest U. S. cities are in California",
+                "The largest tree in the world can be found in California",
+                "California became a state in 1850"
         )
     }
 
